d2f9c9596faaf78ffd52f0cab743820ce1066f6e
Nangongzui <wenbiyou@163.com>
2021-03-17 17:28:06 +0800

    多页面引用&分包

---
+++ webpack-multiple-entry/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    other: "./src/other.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack Indx Page",
+      template: "./public/index.html",
+      filename: "index.html",
+      chunks: ["index"]
+    }),
+    new HtmlWebpackPlugin({
+      title: "Webpack Other Page",
+      template: "./public/other.html",
+      filename: "other.html",
+      chunks: ['other']
+    }),
+  ],
+  devServer: {
+    open: true,
+    port: 9000,
+    proxy: {
+      "/api": {
+        target: "https://api.github.com",
+        pathRewrite: {
+          "^/api": "",
+        },
+        changeOrigin: true,
+      },
+    },
+  },
+};
